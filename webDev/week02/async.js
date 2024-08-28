ffd2413a60345a45131c31d929021a9f22e6cf17
kai <137367860+Itzsoham@users.noreply.github.com>
2024-08-28 22:51:09 +0530

    day 2

--- webDev/week02/async.js
+++ webDev/week02/async.js
@@ -13,3 +13,23 @@ fs.readFile("c.txt", "utf-8", print);
 // Synchronous read
 const contents2 = fs.readFileSync("b.txt", "utf-8");
 console.log(contents2);
+
+///////////////////////////////////////////////////////////////////
+
+function readTheFile(FinalValue){
+  fs.readFile("a.txt", "utf-8", function(err, contents)  {
+    FinalValue(contents);
+  });
+}
+
+function readFile(){
+  return new Promise(readTheFile);
+}
+
+const p = readFile();
+
+function callback(contents){
+  console.log(contents);
+}
+
+p.then(callback);
--- webDev/week02/promises.js
+++ webDev/week02/promises.js
@@ -10,3 +10,36 @@ function callback() {
 
 setTimeoutPromisified(3000).then(callback)
 
+/////////////////////////////////////////////////////////////////////////////////
+
+function waitFor5s(resolve) {
+  setTimeout(resolve, 5000);
+}
+
+function setTimeoutPromisified_Simple(){
+  return new Promise(waitFor5s);
+}
+
+function main() {
+  console.log("5 seconds have passed");
+}
+
+// waitFor3s(main);
+setTimeoutPromisified_Simple().then(main); // promisified version of setTimeout
+setTimeout(main, 5000); // callback version of setTimeout
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+function readFile (filename){
+  return new Promise();
+}
+
+const p = readFile("file.txt");
+
+function onFulfilled(contents){
+  console.log(contents);
+}
+
+p.then(onFulfilled);
