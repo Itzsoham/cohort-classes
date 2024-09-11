a47de14189e2ba394a271650cf6dfb504e3835c0
kai <137367860+Itzsoham@users.noreply.github.com>
2024-09-11 23:05:15 +0530

    Day 16 week 6.1

---
+++ webDev/week06/6.1/index.js
@@ -0,0 +1,139 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const user = [];
+
+function generateToken() {
+  let options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+
+  let token = "";
+  for (let i = 0; i < 32; i++) {
+    // use a simple function here
+    token += options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
+}
+
+app.post("/signing-up", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  user.push({
+    username: username,
+    password: password,
+  });
+  res.json({
+    massege: "User Signed In",
+  });
+});
+
+app.post("/signing-in", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const foundUser = user.find((u) => {
+    if (username == u.username && u.password == password) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  if (foundUser) {
+    const token = generateToken();
+    foundUser.token = token;
+
+    res.json({
+      token: token,
+    });
+    console.log(user);
+  } else {
+    res.status(403).send({
+      message: "Invalid Password and Username",
+    });
+  }
+});
+
+app.get("/me", (req, res) => {
+  const token = req.headers.authorization;
+  const users = user.find((u) => {
+    u.token == token;
+    return true;
+  });
+  if (users) {
+    res.send({
+      username: users.username,
+    });
+  } else {
+    res.status(401).send({
+      message: "Go Home",
+    });
+  }
+});
+
+app.listen(3000);
