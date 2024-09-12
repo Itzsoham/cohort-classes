5f0893f7519ba728980e9da3a2a1923e5fa11814
kai <137367860+Itzsoham@users.noreply.github.com>
2024-09-12 22:56:58 +0530

    Solved jwt error and gitignore

--- webDev/week06/6.1/index.js
+++ webDev/week06/6.1/index.js
@@ -1,83 +1,85 @@
 const express = require("express");
-
+const JWT_SECRET = "whyalwaysfuckeduplikekaizen";
+const jwt = require("jsonwebtoken");
 const app = express();
+
 app.use(express.json());
 
 const user = [];
 
-function generateToken() {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    // use a simple function here
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-}
+// function generateToken() {
+//   let options = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//   ];
+//
+//   let token = "";
+//   for (let i = 0; i < 32; i++) {
+//     // use a simple function here
+//     token += options[Math.floor(Math.random() * options.length)];
+//   }
+//   return token;
+// }
 
 app.post("/signing-up", function (req, res) {
   const username = req.body.username;
@@ -105,8 +107,13 @@ app.post("/signing-in", function (req, res) {
   });
 
   if (foundUser) {
-    const token = generateToken();
-    foundUser.token = token;
+    const token = jwt.sign(
+      {
+        username: username,
+      },
+      JWT_SECRET
+    );
+    // foundUser.token = token;
 
     res.json({
       token: token,
@@ -119,19 +126,41 @@ app.post("/signing-in", function (req, res) {
   }
 });
 
+// app.get("/me", (req, res) => {
+//   const token = req.headers.authorization;
+//   const decodedInfo = jwt.verify(token, JWT_SECRET);
+//   const username = decodedInfo.username;
+//   // const users = user.find((u) => {
+//   //   u.token == token;
+//   //   return true;
+//   // });
+//   const users = user.find((user) => user.username === username);
+
+//   if (users) {
+//     res.send({
+//       username: username,
+//     });
+//   } else {
+//     res.status(401).send({
+//       message: "Go Home",
+//     });
+//   }
+// });
+
 app.get("/me", (req, res) => {
-  const token = req.headers.authorization;
-  const users = user.find((u) => {
-    u.token == token;
-    return true;
-  });
+  const token = req.headers.token;
+  const userDetails = jwt.verify(token, JWT_SECRET);
+
+  const username = userDetails.username;
+  const users = user.find((user) => user.username === username);
+
   if (users) {
     res.send({
       username: users.username,
     });
   } else {
     res.status(401).send({
-      message: "Go Home",
+      message: "Unauthorized",
     });
   }
 });
