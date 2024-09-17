b5b9f339b781f8c870a59c533415a2ece244acf8
kai <137367860+Itzsoham@users.noreply.github.com>
2024-09-17 22:27:38 +0530

    Week 07 end assigment pending

--- webDev/week07/index.js
+++ webDev/week07/index.js
@@ -3,6 +3,7 @@ const { UserModel, TodoModel } = require("./db");
 const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 mongoose.connect(
   "mongodb+srv://itzsoham:hnKRMTmjLh3NO8cA@cluster0.xt2lc.mongodb.net/todo-app"
@@ -15,16 +16,23 @@ app.post("/signup", async function (req, res) {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
+  try {
+    const hasedPassword = await bcrypt.hash(password, 10);
+    await UserModel.create({
+      email: email,
+      password: hasedPassword,
+      name: name,
+    });
 
-  await UserModel.create({
-    email: email,
-    password: password,
-    name: name,
-  });
-
-  res.json({
-    message: "You are signed up",
-  });
+    res.json({
+      message: "You are signed up",
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      message: e.message,
+    });
+  }
 });
 
 app.post("/signin", async function (req, res) {
@@ -36,7 +44,9 @@ app.post("/signin", async function (req, res) {
     password: password,
   });
 
-  if (user) {
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (user && passwordMatch) {
     const token = jwt.sign(
       {
         id: user._id.toString(),
